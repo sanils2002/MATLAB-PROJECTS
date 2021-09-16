68b7938a2aaea72f6af7695fd9c51f44d4f45e50
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-09-16 14:28:06 +0530

    Update combinesignals.m

--- Sound/combinesignals.m
+++ Sound/combinesignals.m
@@ -2,8 +2,10 @@ clear all;
 close all;
 clc;
 
-[wave1,fs1]=audioread('Indian National Anthem.wav'); 
-[wave2,fs2]=audioread('Sri Lankan National Anthem.wav');
+%To plot two audio signals simultaneously...
+
+[wave1,fs1]=audioread('sample1.wav'); 
+[wave2,fs2]=audioread('sample2.wav');
 
 plot(wave1); 
 hold on;
@@ -12,4 +14,4 @@ plot(wave2);
 title('Combined 2 Sound Signals');
 xlabel('Time');
 ylabel('Amplitude');
-legend('Indian National Anthem','Sri Lankan National Anthem');
+legend('Sample1','Sample2');
