4901fe893a071d50cec55ad58f981b24c89c1d0d
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-14 16:44:55 +0530

    Add files via upload

---
+++ Signals/gibbsphenomenon.m
@@ -0,0 +1,49 @@
+clear all;
+close all;
+clc;
+
+syms t;
+n=input('Maximum Limit of k:');
+w=input('Angular Frequency:');
+T=2*pi/w;
+q=input('Function in t:');
+y=inline(q);
+a=[];
+i=1;
+
+for k=-n:n
+  x=@(t)(y(t).*exp(-j*k*w*t));
+  a(i)=integral(x,0,T)/T;
+  i=i+1;
+endfor
+
+l=1;
+p=-n:n;
+i=1;
+x1=[];
+k=1;
+
+for t=0:0.1:10
+  x1(k)=0;
+  for i = l:2*n+1
+    x1(k)=x1(k)+a(i)*exp(-j*k*w*t);
+  endfor
+endfor
+
+subplot(3,1,1);
+stem((-n:n), abs(a));
+title('Magnitude');
+xlabel('k');
+ylabel('a(k)');
+
+subplot(3,1,2);
+stem((-n:n), angle(a));
+title('Phase');
+xlabel('k');
+ylabel('Phase a(k)');
+
+subplot(3,1,3);
+stem((0:0.1:10), x1);
+title('Phase');
+xlabel('k');
+ylabel('Phase a(k)');
