554657e6e40a989c2ad46828fec930b78bbd619c
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-14 16:55:30 +0530

    Add files via upload

---
+++ Signals/fouriertransform.m
@@ -0,0 +1,42 @@
+clear all;
+close all;
+clc;
+
+syms t,w;
+x=input('Function in t:');
+y=inline(x);
+Y1=@(t,w)y(t).*exp(-j*w*t);
+a=input('Lower Time Limit:');
+b=input('Upper Time Limit:');
+n=input('Maximum Limit of w:');
+X=@(w)integral(@(t)Y1(t,w),a,b);
+i=1;
+
+for w=-n:n
+  A(i)=X(w);
+  i=i+1;
+endfor
+
+l=1;
+p=-n:n;
+i=1;
+x1=[];
+k=1;
+
+subplot(1,2,1);
+stem((-n:n), abs(A));
+title('Magnitude');
+xlabel('w');
+ylabel('X(w)');
+
+subplot(1,2,2);
+stem((-n:n), angle(A));
+title('Phase');
+xlabel('w');
+ylabel('Phase X(w)');
+
+subplot(3,1,3);
+stem((0:0.1:10), x1);
+title('Phase');
+xlabel('k');
+ylabel('Phase a(k)');
